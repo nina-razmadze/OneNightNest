70989f6cd36377930db8f20a9d48976ee76fcf38
nina-razmadze <123087063+nina-razmadze@users.noreply.github.com>
2023-10-11 21:38:25 +0400

    selects

---
+++ src/components/Forms/SearchForm/SearchForm.tsx
@@ -0,0 +1,24 @@
+export default function SearchForm() {
+  return (
+    <div>
+      <select>
+        <option></option>
+        <option></option>
+        <option></option>
+        <option></option>
+      </select>
+      <select>
+        <option></option>
+        <option></option>
+        <option></option>
+        <option></option>
+      </select>
+      <select>
+        <option></option>
+        <option></option>
+        <option></option>
+        <option></option>
+      </select>
+    </div>
+  );
+}
