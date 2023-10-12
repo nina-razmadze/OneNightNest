922451a795f983fa2ee5a1647ffc7e7fbbddb593
nina-razmadze <123087063+nina-razmadze@users.noreply.github.com>
2023-10-12 23:05:26 +0400

    LazyImport

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import "./App.css";
 import HomeView from "./views/HomeView/HomeView";
-
+import React, { lazy, Suspense } from "react";
+const RegisterView = lazy(
+  () => import("./views/AuthView/RegisterView/RegisterView")
+);
 function App() {
-  return <HomeView />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <HomeView />
+    </Suspense>
+  );
 }
 
 export default App;
