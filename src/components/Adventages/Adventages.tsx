6ef63ec349f765829149c43edaca9731d3744839
nina-razmadze <123087063+nina-razmadze@users.noreply.github.com>
2023-10-12 22:24:09 +0400

    WebAdvantages

--- src/components/Adventages/Adventages.tsx
+++ src/components/Adventages/Adventages.tsx
@@ -11,24 +11,29 @@ export default function Adventages() {
     // </div>
     <div className="relative bg-secondary-default  p-30 mt-[130px] mb-[100px] py-6 w-full flex center items-center m-auto bg-primary-light">
       <div className="w-[1410px] flex center items-center m-auto">
-        <div className="grid grid-cols-3 gap-4 w-full relative z-10 items-center  rounded-full bg-fcf5eb text-base">
-          <div className="flex flex-col justify-between p-4 leading-normal bg-white text-black">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight">ფასი</h5>
+        <div className="grid grid-cols-3 gap-4 w-full relative z-10 items-center py-12 rounded-full bg-fcf5eb text-base">
+          <div className="flex flex-col justify-between p-4 leading-normal bg-primary-light rounded-lg text-black py-8">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight pb-4">
+              ფასი
+            </h5>
             <p className="mb-3 font-normal">
               მიიღე ყველაზე იაფინი შეთავაზებები საქართველოში
             </p>
           </div>
 
-          <div className="flex flex-col justify-between p-4 leading-normal bg-white text-black">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight">ფასი</h5>
-            <p className="mb-3 font-normal">
-              მიიღე ყველაზე იაფინი შეთავაზებები საქართველოში
-            </p>
+          <div className="flex flex-col justify-between p-4 leading-normal rounded-lg bg-primary-light text-black py-8">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight pb-4">
+              სწრაფი
+            </h5>
+            <p className="mb-3 font-normal">ბინის დაჯავშნა არაუმეტეს 5 წუთში</p>
           </div>
-          <div className="flex flex-col justify-between p-4 leading-normal bg-white text-black">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight">ფასი</h5>
+
+          <div className="flex flex-col justify-between p-4 leading-normal  bg-primary-light rounded-lg text-black py-8">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight pb-4">
+              მარტივი
+            </h5>
             <p className="mb-3 font-normal">
-              მიიღე ყველაზე იაფინი შეთავაზებები საქართველოში
+              ინტუიტიური და მარტივად აღსაქმელი აპლიკაცია
             </p>
           </div>
         </div>
