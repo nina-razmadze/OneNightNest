25de8cd3b82297a92dce90cd92e3a9a2adaca009
nina-razmadze <123087063+nina-razmadze@users.noreply.github.com>
2023-10-12 14:36:48 +0400

    contentLine

--- src/components/ContentImage/ImageLines/LeftImageLine.tsx
+++ src/components/ContentImage/ImageLines/LeftImageLine.tsx
@@ -2,7 +2,7 @@ import roomThree from "../../../images/RoomThree.jpg";
 
 export default function LeftImageLine() {
   return (
-    <div className="bg-secondary-default rounded-lg  flex justify-center mb-16  ">
+    <div className="bg-secondary-default rounded-lg  flex justify-center mb-[60px] relative ">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-[1410px] h-[550px]  ">
         <div className="ml-[30px]  mt-[25px]">
           <img
@@ -11,14 +11,15 @@ export default function LeftImageLine() {
             alt=""
           />
         </div>
-        <div className="flex flex-col justify-between p-4 leading-normal max-w-screen-xl mx-auto">
-          <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Apartments per day
+        <div className="flex flex-col p-4  max-w-screen-xl mx-auto mt-[120px] text-black items-center">
+          <h1>Apartments per day Choose</h1>
+          <h2 className="mb-2 text-2xl font-bold tracking-tight mt-[20px]">
+            an interesting place for you and plan an unforgettable trip. Book an
+            apartment for any time and receive discount cards as a gift.
           </h2>
-          <h3 className="mb-3 font-normal text-black dark:text-gray-400">
-            Choose an interesting place for you and plan an unforgettable trip.
-            Book an apartment for any time and receive discount cards as a gift.
-          </h3>
+          <button className="text-white mt-[20px] bg-secondary-dark font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 cursor-pointer">
+            Get started
+          </button>
         </div>
       </div>
     </div>
