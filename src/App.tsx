c71065156813e4e1650fbd8b9bea2b22dc383a41
nina-razmadze <123087063+nina-razmadze@users.noreply.github.com>
2023-10-13 00:50:51 +0400

    Register

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import "./App.css";
 import React, { lazy, Suspense } from "react";
 
 import HomeView from "./views/HomeView/HomeView";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import RegisterView from "./views/AuthView/RegisterView/RegisterView";
 // const RegisterView = lazy(
 //   () => import("./views/AuthView/RegisterView/RegisterView")
@@ -11,12 +16,10 @@ function App() {
   return (
     <Router>
       <Suspense fallback={<div>Loading...</div>}>
-        <div>
-          <HomeView />
-          <Routes>
-            <Route path="/SignUp" element={<RegisterView />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/" element={<HomeView />} />
+          <Route path="/SignUp" element={<RegisterView />} />
+        </Routes>
       </Suspense>
     </Router>
   );
--- src/views/AuthView/RegisterView/RegisterView.tsx
+++ src/views/AuthView/RegisterView/RegisterView.tsx
@@ -1,26 +1,15 @@
 export default function RegisterView() {
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
+    <section className=" bg-secondary-light min-h-screen w-screen f ">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <a
-          href="#"
-          className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-        >
-          <img
-            className="w-8 h-8 mr-2"
-            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-            alt="logo"
-          />
-          Flowbite
-        </a>
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full  rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 bg-secondary-light">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-black md:text-2xl ">
               Create and account
             </h1>
             <form className="space-y-4 md:space-y-6" action="#">
               <div>
-                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                <label className="block mb-2 text-sm font-medium text-black ">
                   Your email
                 </label>
                 <input
@@ -32,7 +21,7 @@ export default function RegisterView() {
                 />
               </div>
               <div>
-                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                <label className="block mb-2 text-sm font-medium  text-black">
                   Password
                 </label>
                 <input
@@ -40,11 +29,11 @@ export default function RegisterView() {
                   name="password"
                   id="password"
                   placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-300  text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 />
               </div>
               <div>
-                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                <label className="block mb-2 text-sm font-medium  text-black ">
                   Confirm password
                 </label>
                 <input
@@ -64,8 +53,8 @@ export default function RegisterView() {
                     className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
                   />
                 </div>
-                <div className="ml-3 text-sm">
-                  <label className="font-light text-gray-500 dark:text-gray-300">
+                <div className="ml-3 text-sm text-black">
+                  <label className="font-light">
                     I accept the{" "}
                     <a
                       className="font-medium text-primary-600 hover:underline dark:text-primary-500"
@@ -78,11 +67,11 @@ export default function RegisterView() {
               </div>
               <button
                 type="submit"
-                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                className="w-full  text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
-                Create an account
+                Log In
               </button>
-              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+              <p className="text-sm font-light text-black">
                 Already have an account?{" "}
                 <a
                   href="#"
