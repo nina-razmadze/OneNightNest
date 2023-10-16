10a12aa6c25d1cac6c345f5706b1f27a443ba0a4
nina-razmadze <123087063+nina-razmadze@users.noreply.github.com>
2023-10-16 14:48:50 +0400

    finallSideBaqr

--- src/views/User/User.tsx
+++ src/views/User/User.tsx
@@ -16,25 +16,23 @@ export default function User() {
       >
         <div className="h-full px-3 py-4 overflow-y-auto bg-secondary-dark">
           <ul className="space-y-2 font-medium">
-            <li className="list-none no-underline	 mb-0">
+            <li className="list-none no-underline mb-0 flex items-center justify-center ">
               <a
                 href="#"
-                className="flex items-center p-2  text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover-bg-gray-700 group no-underline"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover-bg-gray-700 group no-underline"
               >
                 <svg
-                  className="w-5 h-5 text-gray-500 transition duration-75  "
+                  className="w-5 h-5 text-gray-500 transition duration-75"
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="currentColor"
                   viewBox="0 0 22 21"
-                >
-                  {/* <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
-                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" /> */}
-                  <GiPapers style={{ color: "white", fontSize: "22px" }} />
-                </svg>
-                <span className="ml-3">NNO.ge</span>
+                ></svg>
+
+                <span className="py-2 px-4 text-2xl ">NNO.ge</span>
               </a>
             </li>
+
             <li className="list-none no-underline	 mb-0">
               <a
                 href="#"
