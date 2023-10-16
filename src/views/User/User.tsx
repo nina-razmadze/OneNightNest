664a21578a81977ee24905b1e550f3e189d6d9af
nina-razmadze <123087063+nina-razmadze@users.noreply.github.com>
2023-10-16 14:44:42 +0400

    sideBar

--- src/views/User/User.tsx
+++ src/views/User/User.tsx
@@ -1,4 +1,4 @@
-import { BiHome } from "react-icons/bi";
+import { GiPapers } from "react-icons/gi";
 
 import { BsFillHouseAddFill } from "react-icons/bs";
 import { FaHotel } from "react-icons/fa";
@@ -19,7 +19,7 @@ export default function User() {
             <li className="list-none no-underline	 mb-0">
               <a
                 href="#"
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover-bg-gray-700 group no-underline"
+                className="flex items-center p-2  text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover-bg-gray-700 group no-underline"
               >
                 <svg
                   className="w-5 h-5 text-gray-500 transition duration-75  "
@@ -30,11 +30,48 @@ export default function User() {
                 >
                   {/* <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
                   <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" /> */}
-                  <BsFillHouseAddFill
-                    style={{ color: "white", fontSize: "22px" }}
-                  />
+                  <GiPapers style={{ color: "white", fontSize: "22px" }} />
                 </svg>
-                <span className="ml-3">განცხადების დამატება</span>
+                <span className="ml-3">NNO.ge</span>
+              </a>
+            </li>
+            <li className="list-none no-underline	 mb-0">
+              <a
+                href="#"
+                className="flex items-center p-2  text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover-bg-gray-700 group no-underline"
+              >
+                <svg
+                  className="w-5 h-5 text-gray-500 transition duration-75  "
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 22 21"
+                >
+                  {/* <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
+                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" /> */}
+                  <GiPapers style={{ color: "white", fontSize: "22px" }} />
+                </svg>
+                <span className="ml-3">გვერდები</span>
+              </a>
+            </li>
+
+            <li className="list-none no-underline	 mb-0">
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover-bg-gray-700 group no-underline"
+              >
+                <svg
+                  className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 18 18"
+                ></svg>
+                <BsFillHouseAddFill />
+
+                <span className="flex-1 ml-3 whitespace-nowrap">
+                  განცხ. დამატება
+                </span>
               </a>
             </li>
             <li className="list-none no-underline	 mb-0">
