e492feb2ea1f7d14b1afe97d2061e04f801e9ec1
nina-razmadze <123087063+nina-razmadze@users.noreply.github.com>
2023-10-17 14:50:41 +0400

    Table

--- src/views/User/Statements/Statements.tsx
+++ src/views/User/Statements/Statements.tsx
@@ -2,9 +2,9 @@ export default function statements() {
   return (
     <div className="flex justify-center items-center pr-12 ml-80 pt-32">
       <div className="w-full max-w-6xl p-4">
-        <div className="relative overflow-x-auto  bg-secondary-default">
-          <table className="w-full text-sm text-left  ">
-            <thead className="text-xs text-gray-700 uppercase bg-secondary-dark ">
+        <div className="relative overflow-x-auto">
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 bg-secondary-dark dark:text-gray-400">
               <tr>
                 <th scope="col" className="px-6 py-3">
                   Product name
@@ -15,30 +15,35 @@ export default function statements() {
                 <th scope="col" className="px-6 py-3">
                   Category
                 </th>
+                <th scope="col" className="px-6 py-3">
+                  Price
+                </th>
               </tr>
             </thead>
             <tbody>
-              <tr className=" border-b">
+              <tr className=" border-b bg-secondary-default  dark:border-gray-700">
                 <th
                   scope="row"
-                  className="px-6 py-4 font-medium  whitespace-nowrap dark:text-white"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                 >
                   Apple MacBook Pro 17"
                 </th>
                 <td className="px-6 py-4">Silver</td>
                 <td className="px-6 py-4">Laptop</td>
+                <td className="px-6 py-4">$2999</td>
               </tr>
-              <tr className=" border-b ">
+              <tr className=" border-b bg-secondary-default  dark:border-gray-700">
                 <th
                   scope="row"
-                  className="px-6 py-4 font-medium whitespace-nowrap dark:text-white"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                 >
                   Microsoft Surface Pro
                 </th>
                 <td className="px-6 py-4">White</td>
                 <td className="px-6 py-4">Laptop PC</td>
+                <td className="px-6 py-4">$1999</td>
               </tr>
-              <tr className="bg-white ">
+              <tr className="bg-secondary-default ">
                 <th
                   scope="row"
                   className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
@@ -47,47 +52,7 @@ export default function statements() {
                 </th>
                 <td className="px-6 py-4">Black</td>
                 <td className="px-6 py-4">Accessories</td>
-              </tr>
-
-              <tr className="bg-white ">
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium  whitespace-nowrap dark:text-white"
-                >
-                  Product 4
-                </th>
-                <td className="px-6 py-4">Blue</td>
-                <td className="px-6 py-4">Accessory</td>
-              </tr>
-              <tr className="bg-white">
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium whitespace-nowrap dark:text-white"
-                >
-                  Product 2
-                </th>
-                <td className="px-6 py-4">Blu</td>
-                <td className="px-6 py-4">Accessory</td>
-              </tr>
-              <tr className="bg-white">
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium whitespace-nowrap dark:text-white"
-                >
-                  Product 2
-                </th>
-                <td className="px-6 py-4">Blu</td>
-                <td className="px-6 py-4">Accessory</td>
-              </tr>
-              <tr className="bg-white ">
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium  whitespace-nowrap dark:text-white"
-                >
-                  Product 2
-                </th>
-                <td className="px-6 py-4">Blu</td>
-                <td className="px-6 py-4">Accessory</td>
+                <td className="px-6 py-4">$99</td>
               </tr>
             </tbody>
           </table>
