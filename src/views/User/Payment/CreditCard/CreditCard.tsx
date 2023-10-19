cff83aec39db1444b661a395619ee84edf97a791
nina-razmadze <123087063+nina-razmadze@users.noreply.github.com>
2023-10-20 01:09:01 +0400

    CardAnd Input

--- src/views/User/Payment/CreditCard/CreditCard.tsx
+++ src/views/User/Payment/CreditCard/CreditCard.tsx
@@ -13,7 +13,7 @@ export default function CreditCard() {
     <div className="flex justify-center items-center pr-12 ml-80 pt-32">
       <div className="w-full max-w-6xl p-4 bg-secondary-dark rounded-lg">
         <div className="grid grid-cols-2 gap-4">
-          <div className="align-items">
+          <div className=" flex items-center">
             <Cards
               number={number}
               name={name}
@@ -28,85 +28,65 @@ export default function CreditCard() {
             <form>
               <div>
                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                  Your email
+                  Card Number
                 </label>
                 <input
                   type="tel"
                   name="number"
                   value={number}
-                  placeholder="Card Number"
+                  placeholder="XXXX XXXX XXXX XXXX"
                   onChange={(e) => setNumber(e.target.value)}
                   onFocus={() => setFocus("number")}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-secondary-default border-none text-gray-900 sm:text-sm rounded-lg  block w-full p-2.5 "
                 />
               </div>
               <div>
                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                  Password
+                  Name
                 </label>
                 <input
                   type="text"
                   name="name"
                   value={name}
-                  placeholder="Name"
                   onChange={(e) => setName(e.target.value)}
                   onFocus={() => setFocus("name")}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-secondary-default border-none text-gray-900 sm:text-sm rounded-lg  block w-full p-2.5 "
                 />
               </div>
+
               <div>
                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                  Confirm password
+                  MM/YY Expiry
                 </label>
                 <input
                   type="text"
                   name="expiry"
                   value={expiry}
-                  placeholder="MM/YY Expiry"
+                  placeholder="MM/YY"
                   onChange={(e) => setExpiry(e.target.value)}
                   onFocus={() => setFocus("expiry")}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-secondary-default border-none text-gray-900 sm:text-sm rounded-lg  block w-full p-2.5 "
                 />
               </div>
-              <div className="flex items-start">
-                <div className="flex items-center h-5">
-                  <input
-                    type="tel"
-                    name="cvc"
-                    value={cvc}
-                    placeholder="CVC"
-                    onChange={(e) => setCvc(e.target.value)}
-                    onFocus={() => setFocus("cvc")}
-                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                  />
-                </div>
-                <div className="ml-3 text-sm">
-                  <label className="font-light text-gray-500 dark:text-gray-300">
-                    I accept the
-                    <a
-                      className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                      href="#"
-                    >
-                      Terms and Conditions
-                    </a>
-                  </label>
-                </div>
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  MM/YY Expiry
+                </label>
+                <input
+                  type="tel"
+                  name="cvc"
+                  value={cvc}
+                  placeholder="CVC"
+                  onChange={(e) => setCvc(e.target.value)}
+                  onFocus={() => setFocus("cvc")}
+                  className="bg-secondary-default border-none text-gray-900 sm:text-sm rounded-lg  block w-full p-2.5 "
+                />
               </div>
-              <button
-                type="submit"
-                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-              >
-                Create an account
-              </button>
-              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                Already have an account?{" "}
-                <a
-                  href="#"
-                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                >
-                  Login here
-                </a>
-              </p>
+              <div className="flex items-center justify-end mt-6">
+                <button className="text-secondary-dark  bg-secondary-default  font-medium rounded-full border-none text-sm px-5 py-2.5 text-center mr-2 mb-2 cursor-pointer">
+                  Get started
+                </button>
+              </div>
             </form>
           </div>
         </div>
