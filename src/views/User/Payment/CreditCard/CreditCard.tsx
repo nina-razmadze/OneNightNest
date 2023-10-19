d78495e1c39c78f7dff85a2a5fe5a21a8c8baae0
nina-razmadze <123087063+nina-razmadze@users.noreply.github.com>
2023-10-20 00:01:48 +0400

    card

--- src/views/User/Payment/CreditCard/CreditCard.tsx
+++ src/views/User/Payment/CreditCard/CreditCard.tsx
@@ -1,7 +1,5 @@
-import "react-credit-cards/es/styles-compiled.css";
-
 import Cards from "react-credit-cards";
-
+import "react-credit-cards/es/styles-compiled.css";
 import { useState } from "react";
 
 export default function CreditCard() {
@@ -9,17 +7,17 @@ export default function CreditCard() {
   const [name, setName] = useState("");
   const [expiry, setExpiry] = useState("");
   const [cvc, setCvc] = useState("");
-  const [focus, setfocus] = useState("");
+  const [focus, setFocus] = useState("");
 
   return (
-    <div className="flex justify-center items-center  pr-12 ml-80 pt-32">
-      <div className="w-full max-w-6xl p-4  bg-secondary-dark rounded-lg">
+    <div className="flex justify-center items-center pr-12 ml-80 pt-32">
+      <div className="w-full max-w-6xl p-4 bg-secondary-dark rounded-lg">
         <Cards
           number={number}
           name={name}
           expiry={expiry}
           cvc={cvc}
-          // focused={focus}
+          focused={focus}
         />
         <form>
           <input
@@ -28,7 +26,7 @@ export default function CreditCard() {
             value={number}
             placeholder="Card Number"
             onChange={(e) => setNumber(e.target.value)}
-            onFocus={(e) => setfocus(e.target.name)}
+            onFocus={() => setFocus("number")}
           />
           <input
             type="text"
@@ -36,7 +34,7 @@ export default function CreditCard() {
             value={name}
             placeholder="Name"
             onChange={(e) => setName(e.target.value)}
-            onFocus={(e) => setfocus(e.target.name)}
+            onFocus={() => setFocus("name")}
           />
           <input
             type="text"
@@ -44,7 +42,7 @@ export default function CreditCard() {
             value={expiry}
             placeholder="MM/YY Expiry"
             onChange={(e) => setExpiry(e.target.value)}
-            onFocus={(e) => setfocus(e.target.name)}
+            onFocus={() => setFocus("expiry")}
           />
           <input
             type="tel"
@@ -52,97 +50,107 @@ export default function CreditCard() {
             value={cvc}
             placeholder="CVC"
             onChange={(e) => setCvc(e.target.value)}
-            onFocus={(e) => setfocus(e.target.name)}
+            onFocus={() => setFocus("cvc")}
           />
         </form>
+
+        <section className="bg-gray-50 dark:bg-gray-900">
+          <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <a
+              href="#"
+              className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+            >
+              <img
+                className="w-8 h-8 mr-2"
+                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+                alt="logo"
+              />
+              Flowbite
+            </a>
+            <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+              <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                  Create and account
+                </h1>
+                <form className="space-y-4 md:space-y-6" action="#">
+                  <div>
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      Your email
+                    </label>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="name@company.com"
+                    />
+                  </div>
+                  <div>
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      placeholder="••••••••"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    />
+                  </div>
+                  <div>
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      Confirm password
+                    </label>
+                    <input
+                      type="confirm-password"
+                      name="confirm-password"
+                      id="confirm-password"
+                      placeholder="••••••••"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    />
+                  </div>
+                  <div className="flex items-start">
+                    <div className="flex items-center h-5">
+                      <input
+                        id="terms"
+                        aria-describedby="terms"
+                        type="checkbox"
+                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                      />
+                    </div>
+                    <div className="ml-3 text-sm">
+                      <label className="font-light text-gray-500 dark:text-gray-300">
+                        I accept the{" "}
+                        <a
+                          className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                          href="#"
+                        >
+                          Terms and Conditions
+                        </a>
+                      </label>
+                    </div>
+                  </div>
+                  <button
+                    type="submit"
+                    className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                  >
+                    Create an account
+                  </button>
+                  <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                    Already have an account?{" "}
+                    <a
+                      href="#"
+                      className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                    >
+                      Login here
+                    </a>
+                  </p>
+                </form>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
-
-    // <div className="flex justify-center items-center  pr-12 ml-80 pt-32 ">
-    //   <div className="p-4 ">
-    //     <h1 className="pb-12">ბარათის მონაცემები</h1>
-    //     <div className=" border shadow bg-secondary-default rounded-lg p-8 ">
-    //       <form>
-    //         <div className="mb-6">
-    //           <label className="block mb-2 text-sm font-medium text-black ">
-    //             Name
-    //           </label>
-    //           <input
-    //             type="text"
-    //             placeholder=""
-    //             id="base-input"
-    //             className="bg-transparent  text-gray-900 text-sm rounded-lg  block w-full p-2.5  "
-    //           />
-    //         </div>
-    //         <div className="mb-6">
-    //           <label className="block mb-2 text-sm font-medium text-black ">
-    //             Card Number
-    //           </label>
-    //           <input
-    //             type="text"
-    //             placeholder="XXXX XXXX XXXX XXXX"
-    //             id="base-input"
-    //             className="bg-transparent  text-gray-900 text-sm rounded-lg  block w-full p-2.5  "
-    //           />
-    //         </div>
-
-    //         <div className="grid md:grid-cols-3 md:gap-6">
-    //           <div className="relative z-0 w-full mb-6 group">
-    //             <label
-    //               className="block text-sm text-gray-500"
-    //               htmlFor="expiryDate"
-    //             >
-    //               Expiry Date
-    //             </label>
-    //             <input
-    //               type="text"
-    //               name="expiryDate"
-    //               id="expiryDate"
-    //               className="block w-full py-2.5 px-4 text-sm text-gray-900 bg-transparent  rounded-lg focus:outline-none focus:border-blue-500"
-    //               placeholder="MM/YY"
-    //               required
-    //             />
-    //           </div>
-    //           <div className="relative z-0 w-full mb-6 group">
-    //             <label
-    //               className="block text-sm text-gray-500"
-    //               htmlFor="expiryDate"
-    //             >
-    //               Expiry Date
-    //             </label>
-    //             <input
-    //               type="text"
-    //               name="expiryDate"
-    //               id="expiryDate"
-    //               className="block w-full py-2.5 px-4 text-sm text-gray-900 bg-transparent  rounded-lg focus:outline-none focus:border-blue-500"
-    //               placeholder="MM/YY"
-    //               required
-    //             />
-    //           </div>
-    //           <div className="relative z-0 w-full mb-6 group">
-    //             <label
-    //               className="block text-sm text-gray-500"
-    //               htmlFor="expiryDate"
-    //             >
-    //               CVV
-    //             </label>
-    //             <input
-    //               type="text"
-    //               name="expiryDate"
-    //               id="expiryDate"
-    //               className="block w-full py-2.5 px-4 text-sm text-gray-900 bg-transparent  rounded-lg focus:outline-none focus:border-blue-500"
-    //               placeholder="MM/YY"
-    //               required
-    //             />
-    //           </div>
-    //         </div>
-
-    //         <button className="text-white  bg-secondary-dark font-medium rounded-full border-none text-sm px-5 py-2.5 text-center mr-2 mb-2 cursor-pointer">
-    //           Submit
-    //         </button>
-    //       </form>
-    //     </div>
-    //   </div>
-    // </div>
   );
 }
