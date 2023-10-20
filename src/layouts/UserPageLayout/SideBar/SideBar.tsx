f0cbb7e6a2c5154a755ecf352b2f3baa0bc9a281
nina-razmadze <123087063+nina-razmadze@users.noreply.github.com>
2023-10-20 14:30:12 +0400

    ListRutes

--- src/layouts/UserPageLayout/SideBar/SideBar.tsx
+++ src/layouts/UserPageLayout/SideBar/SideBar.tsx
@@ -17,23 +17,20 @@ export default function SideBar() {
       >
         <div className="h-full px-3 py-4 overflow-y-auto bg-secondary-dark">
           <ul className="space-y-2 font-medium">
-            <li className="list-none mb-0 flex items-center justify-center">
-              <a
-                href="#"
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover-bg-gray-700 group"
-              >
-                <svg
-                  className="w-5 h-5 text-gray-500 transition duration-75"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 22 21"
-                ></svg>
-                <Link to="/">
+            <Link to="/" className="no-underline">
+              <li className="list-none mb-0 flex items-center justify-center">
+                <a className="flex items-center p-2  rounded-lg group">
+                  <svg
+                    className="w-5 h-5  transition duration-75"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 22 21"
+                  ></svg>
                   <span className="py-2 px-4 text-2xl text-white">NNO.ge</span>
-                </Link>
-              </a>
-            </li>
+                </a>
+              </li>
+            </Link>
 
             <li className="list-none mb-0 flex items-center p-2  text-white rounded-lg  group no-underline">
               <svg
@@ -48,52 +45,58 @@ export default function SideBar() {
               <span className="ml-3">გვერდები</span>
             </li>
 
-            <li className="list-none no-underline	 mb-0 flex items-center p-2  rounded-lg text-white  group ">
-              <svg
-                className="flex-shrink-0 w-5 h-5transition duration-75 "
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 18 18"
-              ></svg>
-              <BsFillHouseAddFill />
+            <Link to="/userpage/mylist" className="no-underline">
+              <li className="list-none  mb-0 flex items-center p-2  rounded-lg text-white  group ">
+                <svg
+                  className="flex-shrink-0 w-5 h-5transition duration-75 "
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 18 18"
+                ></svg>
+                <BsFillHouseAddFill />
 
-              <span className="flex-1 ml-3 whitespace-nowrap">
-                განცხად. დამატება
-              </span>
-            </li>
+                <span className="flex-1 ml-3 whitespace-nowrap">
+                  განცხად. დამატება
+                </span>
+              </li>
+            </Link>
 
-            <li className="list-none no-underline	 mb-0 flex items-center p-2  rounded-lg text-white  group ">
-              <svg
-                className="flex-shrink-0 w-5 h-5transition duration-75 "
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 18 18"
-              ></svg>
-              <AiOutlineUnorderedList />
+            <Link to="/userpage/mylist" className="no-underline">
+              <li className="list-none  mb-0 flex items-center p-2  rounded-lg text-white  group ">
+                <svg
+                  className="flex-shrink-0 w-5 h-5transition duration-75 "
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 18 18"
+                ></svg>
+                <AiOutlineUnorderedList />
 
-              <span className="flex-1 ml-3 whitespace-nowrap">
-                ჩემი განცხადებები
-              </span>
-            </li>
+                <span className="flex-1 ml-3 whitespace-nowrap">
+                  ჩემი განცხადებები
+                </span>
+              </li>
+            </Link>
 
-            <li className="list-none no-underline	 mb-0 flex items-center p-2 text-white rounded-lg  group no-underline">
-              <svg
-                className="flex-shrink-0 w-5 h-5 transition duration-75"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              ></svg>
-              <FaHotel />
-              <span className="flex-1 ml-3 whitespace-nowrap">
-                ჩემი დაჯავშნილები
-              </span>
-            </li>
+            <Link to="/userpage/creditcard" className="no-underline">
+              <li className="	mb-0 flex items-center p-2  rounded-lg text-white  group ">
+                <svg
+                  className="flex-shrink-0 w-5 h-5 transition duration-75"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                ></svg>
+                <FaHotel />
+                <span className="flex-1 ml-3 whitespace-nowrap">
+                  ჩემი დაჯავშნილები
+                </span>
+              </li>
+            </Link>
 
-            <Link to="/userpage/payment" className="no-underline">
-              <li className="list-none mb-0 flex items-center p-2 text-white rounded-lg group">
+            <Link to="/userpage/creditcard" className="no-underline">
+              <li className="list-none 	 mb-0 flex items-center p-2  rounded-lg text-white  group ">
                 <svg
                   className="flex-shrink-0 w-5 h-5 transition duration-75"
                   aria-hidden="true"
