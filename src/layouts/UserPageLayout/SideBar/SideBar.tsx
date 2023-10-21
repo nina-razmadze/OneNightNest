e79b24de07593c989879993b429fa94f28434ae4
nina-razmadze <123087063+nina-razmadze@users.noreply.github.com>
2023-10-21 22:21:09 +0400

    Routes

--- src/layouts/UserPageLayout/SideBar/SideBar.tsx
+++ src/layouts/UserPageLayout/SideBar/SideBar.tsx
@@ -98,21 +98,21 @@ export default function SideBar() {
               </li>
             </Link>
 
-            <Link to="/userpage/creditcard" className="no-underline">
-              <li className="	mb-0 flex items-center p-2  rounded-lg text-white  group ">
-                <svg
-                  className="flex-shrink-0 w-5 h-5 transition duration-75"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                ></svg>
-                <FaHotel />
-                <span className="flex-1 ml-3 whitespace-nowrap">
-                  ჩემი დაჯავშნილები
-                </span>
-              </li>
-            </Link>
+            {/* <Link to="/userpage/creditcard" className="no-underline"> */}
+            <li className="	mb-0 flex items-center p-2  rounded-lg text-white  group ">
+              <svg
+                className="flex-shrink-0 w-5 h-5 transition duration-75"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              ></svg>
+              <FaHotel />
+              <span className="flex-1 ml-3 whitespace-nowrap">
+                ჩემი დაჯავშნილები
+              </span>
+            </li>
+            {/* </Link> */}
 
             <Link to="/userpage/creditcard" className="no-underline">
               <li className="list-none 	 mb-0 flex items-center p-2  rounded-lg text-white  group ">
